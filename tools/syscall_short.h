4c48380cf950b632b65a9cc1d8f4dd4073780d20
Shani <Shani>
2015-04-27 21:40:44 +0300

    added .h file

---
+++ tools/syscall_short.h
@@ -0,0 +1,53 @@
+#include <errno.h>
+
+int is_SHORT(int pid) {
+	long __res;
+	__asm__ volatile (
+	"movl $243, %%eax;"
+	"movl %1, %%ebx;"
+	"int $0x80;"
+	"movl %%eax,%0"
+	: "=m" (__res) 		/* output operands */
+	: "m" (maxp)		/* input operands */
+	: "%eax","%ebx"		/* list of clobbered registers */
+	);
+	if ((unsigned long)(__res) >= (unsigned long)(-125)) {
+	errno = -(__res); __res = -1;
+	}
+	return (int)(__res);
+}
+
+int remaining_time(int pid) {
+	long __res;
+	__asm__ volatile (
+	"movl $244, %%eax;"
+	"movl %1, %%ebx;"	
+	"int $0x80;"
+	"movl %%eax,%0"
+	: "=m" (__res)		/* output operands */
+	:			/* input operands */
+	: "%eax"		/* list of clobbered registers */
+	);
+	if ((unsigned long)(__res) >= (unsigned long)(-125)) {
+	errno = -(__res); __res = -1;
+	}
+	return (int)(__res);
+}
+
+
+int remaining_trails(int pid) {
+	long __res;
+	__asm__ volatile (
+	"movl $245, %%eax;"
+	"movl %1, %%ebx;"
+	"int $0x80;"
+	"movl %%eax,%0"
+	: "=m" (__res)		/* output operands */
+	:			/* input operands */
+	: "%eax"		/* list of clobbered registers */
+	);
+	if ((unsigned long)(__res) >= (unsigned long)(-125)) {
+	errno = -(__res); __res = -1;
+	}
+	return (int)(__res);
+}
